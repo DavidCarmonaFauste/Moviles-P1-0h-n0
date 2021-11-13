5146fd2a1d659218b9e9930cf6d8598bbe9254e3
David Carmona <davicarm@ucm.es>
2021-11-13 19:40:39 +0100

    Image Class

---
+++ OhnO/Engine/src/main/java/es/ucm/vm/engine/MyImage.java
@@ -0,0 +1,10 @@
+package es.ucm.vm.engine;
+
+public interface MyImage {
+    public void readImage(String filename);
+    public void render();
+    public void setPosition(int x, int y);
+    public void setSize(int x, int y);
+
+
+}
